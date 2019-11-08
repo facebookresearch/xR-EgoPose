26295eb07f92faf5217d4548b1dff706fdf7c0cf
Denis Tome <denis.tome@oculus.com>
2019-11-08 14:21:12 +0000

    demo code for tests

--- demo.py
+++ demo.py
@@ -5,20 +5,58 @@ Demo code
 @author: Denis Tome'
 
 """
+from torch.utils.data import DataLoader
 from torchvision import transforms
 from base import SetType
 import dataset.transform as trsf
 from dataset import Mocap
-from utils import config
+from utils import config, ConsoleLogger
+
+LOGGER = ConsoleLogger("Main")
 
 
-# db = Mocap(config.dataset.val, SetType.VAL)
-data_transform = transforms.Compose([
-    trsf.ImageTrsf(),
-    trsf.Joints3DTrsf(),
-    trsf.ToTensor()
-])
-db = Mocap('data/Dataset/Tmp',
-           SetType.VAL,
-           transform=data_transform)
-db.__getitem__(0)
+def main():
+    """Main"""
+
+    LOGGER.info('Starting demo...')
+
+    # ------------------- Data loader -------------------
+
+    data_transform = transforms.Compose([
+        trsf.ImageTrsf(),
+        trsf.Joints3DTrsf(),
+        trsf.ToTensor()])
+
+    # let's load data from validation set
+    data = Mocap(
+        config.dataset.val,
+        SetType.VAL,
+        transform=data_transform)
+    data_loader = DataLoader(
+        data,
+        batch_size=config.data_loader.batch_size,
+        shuffle=config.data_loader.shuffle)
+
+    # ------------------- Read data -------------------
+
+    for it, (img, p2d, p3d, action) in enumerate(data_loader):
+
+        LOGGER.info('Iteration: {}'.format(it))
+        LOGGER.info('Images: {}'.format(img.shape))
+        LOGGER.info('p2ds: {}'.format(p2d.shape))
+        LOGGER.info('p3ds: {}'.format(p3d.shape))
+        LOGGER.info('Actions: {}'.format(action))
+
+        # -----------------------------------------------------
+        # ------------------- Run you model -------------------
+        # -----------------------------------------------------
+
+        #TODO: add evaluation code
+
+        break
+
+    LOGGER.info('Done')
+
+
+if __name__ == "__main__":
+    main()
--- download.sh
+++ download.sh
@@ -1,27 +1,91 @@
 #!/usr/bin/env bash
 
-root_dir=${PWD}
+dataset_dir=${PWD}/data/Dataset
+declare -a a_test=("female_004_a_a"
+				"female_008_a_a"
+				"female_010_a_a"
+				"female_012_a_a"
+				"female_012_f_s"
+				"male_001_a_a"
+				"male_002_a_a"
+				"male_004_f_s"
+				"male_006_a_a"
+				"male_007_f_s"
+				"male_010_a_a"
+				"male_014_f_s")
+declare -a a_train=("female_001_a_a"
+				"female_002_a_a"
+				"female_002_f_s"
+				"female_003_a_a"
+				"female_005_a_a"
+				"female_006_a_a"
+				"female_007_a_a"
+				"female_009_a_a"
+				"female_011_a_a"
+				"female_014_a_a"
+				"female_015_a_a"
+				"male_003_f_s"
+				"male_004_a_a"
+				"male_005_a_a"
+				"male_006_f_s"
+				"male_007_a_a"
+				"male_008_f_s"
+				"male_009_a_a"
+				"male_010_f_s"
+				"male_011_f_s"
+				"male_014_a_a")
+declare -a a_val=("male_008_a_a")
 
-mkdir -p data/Dataset
-cd data/Dataset
+download_set () {
+	arr=("$@")
 
-# # Abort on error
-# set -e
+	for s in "${arr[@]}"
+	do
 
-# for p in {a..i}
-# do
-#   # Ensure files are continued in case the script gets interrupted halfway through
-#   wget --continue https://github.com/facebookresearch/Replica-Dataset/releases/download/v1.0/replica_v1_0.tar.gz.parta$p
-# done
+        # download all parts for character
+        for p in {a..j}
+        do
+            url=https://github.com/facebookresearch/xR-EgoPose/releases/download/v1.0/$s.tar.gz.parta$p
 
-# # Create the destination directory if it doesn't exist yet
-# mkdir -p $1
+            # Ensure files are continued in case the script gets interrupted halfway through
+            wget --continue $url
+            wgetreturn=$?
+            echo $wgetreturn
+            if [[ $wgetreturn -ne 0 ]]; then
+                break
+            fi
+		done
 
+        # extract data
+        cat $s.tar.gz.part?? | unpigz -p 32  | tar -xvC ./
 
-# download all files...
-# loop trough sets and extract
+        # remove compressed data
+        rm -rf $s.tar.gz*
 
-# tmp let's do a test with tmp / replace with set name (works)
-# cd Tmp
-# cat male_008_a_a.tar.gz.part?? | unpigz -p 32  | tar -xvC ./
-# rm -rf male_008_a_a.tar.gz*
+	done
+}
+
+# Abort on error
+set -e
+
+# Create the destination directory if it doesn't exist yet
+mkdir -p ${dataset_dir}
+cd ${dataset_dir}
+
+# Download and process Validation set
+echo "Downloading and extracting Validation set"
+mkdir -p ValSet
+cd ValSet
+download_set "${a_val[@]}"
+
+# Download and process Train set
+echo "Downloading and extracting Train set"
+mkdir -p TrainSet
+cd TrainSet
+download_set "${a_train[@]}"
+
+# Download and process Test set
+echo "Downloading and extracting Test set"
+mkdir -p TestSet
+cd TestSet
+download_set "${a_test[@]}"
