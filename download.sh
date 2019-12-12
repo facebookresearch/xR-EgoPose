b33ed8eeab31a2e77b8c76ed25db8d8411ea3ae3
Hernan Badino <hernan.badino@oculus.com>
2019-12-12 18:40:29 -0500

    Bug fixes on download

--- download.sh
+++ download.sh
@@ -14,11 +14,12 @@ declare -a a_test=("female_004_a_a"
 				"male_007_f_s"
 				"male_010_a_a"
 				"male_014_f_s")
+declare -a max_test=("i" "i" "i" "f" "a" "i" "j" "a" "i" "a" "i" "a")
 declare -a a_train=("female_001_a_a"
 				"female_002_a_a"
 				"female_002_f_s"
 				"female_003_a_a"
-				"female_005_a_a"
+				"female_005_a_a"	
 				"female_006_a_a"
 				"female_007_a_a"
 				"female_009_a_a"
@@ -35,26 +36,53 @@ declare -a a_train=("female_001_a_a"
 				"male_010_f_s"
 				"male_011_f_s"
 				"male_014_a_a")
+declare -a max_train=("i" "j" "a" "f" "i" "i" "h" "i" "f" "f" "j" "a" "i" "j" "a" "i" "a" "h" "a" "a" "i")
 declare -a a_val=("male_008_a_a")
+declare -a max_val=("i")
 
 download_set () {
-	arr=("$@")
 
-	for s in "${arr[@]}"
+	case "$1" in
+
+		ValSet)
+			echo "ValSet"
+			arr=("${a_val[@]}")
+			max=("${max_val[@]}")
+			;;
+
+		TestSet)
+			echo "TestSet"
+			arr=("${a_test[@]}")
+			max=("${max_test[@]}")
+			;;
+
+		TrainSet)
+			echo "TrainSet"
+			arr=("${a_train[@]}")
+			max=("${max_train[@]}")
+			;;
+
+		*)
+			break
+			;;
+
+	esac
+
+	echo "Downloading and extracting" $1
+	mkdir -p $1
+	cd $1
+
+	for i in "${!arr[@]}"
 	do
-
+		s=${arr[$i]}
+		m=${max[$i]}
         # download all parts for character
-        for p in {a..j}
+        for p in $(eval echo {a..$m})
         do
             url=https://github.com/facebookresearch/xR-EgoPose/releases/download/v1.0/$s.tar.gz.parta$p
 
             # Ensure files are continued in case the script gets interrupted halfway through
             wget --continue $url
-            wgetreturn=$?
-            echo $wgetreturn
-            if [[ $wgetreturn -ne 0 ]]; then
-                break
-            fi
 		done
 
         # extract data
@@ -64,6 +92,8 @@ download_set () {
         rm -rf $s.tar.gz*
 
 	done
+
+	cd ..
 }
 
 # Abort on error
@@ -74,21 +104,10 @@ mkdir -p ${dataset_dir}
 cd ${dataset_dir}
 
 # Download and process Validation set
-echo "Downloading and extracting Validation set"
-mkdir -p ValSet
-cd ValSet
-download_set "${a_val[@]}"
+# download_set "ValSet"
 
 # Download and process Train set
-echo "Downloading and extracting Train set"
-cd ${dataset_dir}
-mkdir -p TrainSet
-cd TrainSet
-download_set "${a_train[@]}"
+download_set "TrainSet"
 
-# Download and process Test set
-echo "Downloading and extracting Test set"
-cd ${dataset_dir}
-mkdir -p TestSet
-cd TestSet
-download_set "${a_test[@]}"
+# # Download and process Test set
+# download_set "TestSet"
